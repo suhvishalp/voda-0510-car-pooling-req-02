cc897069b548ef46e04b1a11ae2b5315a7f9db5d
suhvishalp <suhvishal@gmail.com>
2023-10-10 15:05:55 +0530

    -

--- src/main/java/TestCarPoolingReq2.java
+++ src/main/java/TestCarPoolingReq2.java
@@ -1,9 +1,47 @@
+import java.util.ArrayList;
 
 public class TestCarPoolingReq2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		ArrayList<Member> memberList = new ArrayList<>();
+		ArrayList<Car> carList = new ArrayList<>();
+		
+		
+		//enter choice : 1
+		//create a new Member object
+		//add this member object to the memberList
+		
+		
+		//enter choice: 2
+		//create a new Car object
+		//add this Car object to the carList
+		
+		
+		//enter choice: 3
+		//input carId and memberId & find the Car and Member objects
+		
+		//assign the car to the given member i.e. Member.assignCarToMember(Car)
+				
+		
+	}
+	
+	
+//	public static Member findMember(ArrayList<Member> memberList, int id) {
+//		
+//		//logic to find the member of given id
+//		for(Member member : memberList) {
+//			if(member.getId() == id)
+//				return member;
+//		}
+//		
+//		return null;
+//	}
+	
+	public static Car findCar(ArrayList<Car> carList, int carId) {
+		
+		return null;
 	}
 
 }
